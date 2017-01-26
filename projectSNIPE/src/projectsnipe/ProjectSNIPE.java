c317fde9c0d3f13ef21d26270be8f158027d38a5
Jade <Jade@DESKTOP-G2F7OO8>
2017-01-25 18:51:16 -0700

    1/25/2017 18:51 - Player, Item, Game

--- projectSNIPE/src/projectsnipe/ProjectSNIPE.java
+++ projectSNIPE/src/projectsnipe/ProjectSNIPE.java
@@ -5,7 +5,9 @@
  */
 package projectsnipe;
 
+import byui.cit260.projectSNIPE.model.Item;
 import byui.cit260.projectSNIPE.model.Player;
+import byui.cit260.projectSNIPE.model.Game;
 
 /**
  *
@@ -17,13 +19,26 @@ public class ProjectSNIPE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Player - Class Initialization
         Player playerOne = new Player();
-        
         playerOne.setName("Fred Flintsone");
         playerOne.setBestTime(7.00);
-        
         String playerInfo = playerOne.toString();
         System.out.println(playerInfo);
+        // Item - Class Init
+        Item itemMoney = new Item();
+        itemMoney.setInventoryType("Money");
+        itemMoney.setStock(1000000);
+        itemMoney.setAmount(100.00);
+        String itemMoneyInfo = itemMoney.toString();
+        System.out.println(itemMoney);
+        // Game - Class Init
+        Game gameOne = new Game();
+        gameOne.setNoPeople(0);
+        gameOne.setTotalTime(0);
+        String gameOneInfo = gameOne.toString();
+        System.out.println(gameOne);
+        
     }
     
 }
