ff702d91d11001fb9f1b493bbfcac08fbfa8e93e
Daniel <Daniel@192.168.1.103>
2017-02-16 17:46:50 -0700

    fixed formatting + renamed project for my organization

--- projectSNIPE/src/byui/cit260/projectSNIPE/control/InventoryControl.java
+++ projectSNIPE/src/byui/cit260/projectSNIPE/control/InventoryControl.java
@@ -10,14 +10,15 @@ package byui.cit260.projectSNIPE.control;
  * @author JadeJ
  */
 public class InventoryControl {
-public double calcNewMoney(double money, double exchangeRate){
-    if (money <1 || money >1000){
-        return -1;
+
+    public double calcNewMoney(double money, double exchangeRate) {
+        if (money < 1 || money > 1000) {
+            return -1;
         }
-    if (exchangeRate < 0 || exchangeRate >60){
-        return -1;
+        if (exchangeRate < 0 || exchangeRate > 60) {
+            return -1;
         }
-double newMoney = (money * exchangeRate) * 0.95;
-return newMoney;
-        } 
+        double newMoney = (money * exchangeRate) * 0.95;
+        return newMoney;
+    }
 }
