915e71b0eaf45d4a5aaaff8a3526b31d5ad8a5b0
JadeJ <JadeJ@DESKTOP-H2IH7J4>
2017-02-23 11:42:15 -0700

    Added simple menus to all unused views

--- projectSNIPE/src/byui/cit260/projectSNIPE/view/LocalLocationView.java
+++ projectSNIPE/src/byui/cit260/projectSNIPE/view/LocalLocationView.java
@@ -5,10 +5,73 @@
  */
 package byui.cit260.projectSNIPE.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Jade
  */
 public class LocalLocationView {
-    
+            private String menu;
+    public LocalLocationView(){
+        this.menu = "\n"
+                + "\n----------------"
+                + "\n| Location Menu     |"
+                + "\nM - Main Menu"
+                + "\nH - Help Menu";
+    }
+    void displayGameMenu(){
+       boolean done = false;
+        do{
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("Q"))
+                return;
+            done = this.doAction(menuOption);
+        }
+        while(!done);
+        }
+
+    private String getMenuOption() {
+        Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        boolean valid = false;
+        
+        while (!valid){
+            System.out.println("\n" + this.menu);
+            value = keyboard.nextLine();
+            value = value.trim();
+            if (value.length() < 1){
+                System.out.println("\nInvalid value: value can not be blank");
+                continue;
+        }
+            break;
+        }
+        return value;
+    }
+
+    private boolean doAction(String choice) {
+       choice = choice.toUpperCase();
+        
+        switch (choice){
+            case "M":
+                this.gameMenu();
+                break;
+            case "H":
+                this.helpMenu();
+                break;
+            default:
+                System.out.println("\n***Invalid Selection *** Try Again");
+                break;
+    }
+        return false;
+    }
+      private void gameMenu() {
+       MainMenuView mainMenu = new MainMenuView();
+       mainMenu.displayMainMenuView();
+    }
+
+    private void helpMenu() {
+       HelpMenuView helpMenu = new HelpMenuView();
+        helpMenu.displayHelpMenuView();
+    }
 }
