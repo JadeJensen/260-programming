ae992f7e932b1b0a3c61d057c6a8330da9b8fb96
Jade <Jade@DESKTOP-G2F7OO8>
2017-02-22 18:03:38 -0700

    Added all views for game. No views have been developed yet. -- Jade

--- projectSNIPE/src/byui/cit260/projectSNIPE/view/CreditsView.java
+++ projectSNIPE/src/byui/cit260/projectSNIPE/view/CreditsView.java
@@ -10,5 +10,18 @@ package byui.cit260.projectSNIPE.view;
  * @author Jade
  */
 public class CreditsView {
-    
-}
+
+    public CreditsView() {
+        this.displayBanner();
+        
+    }
+         private void displayBanner() {
+       System.out.println("/n===================================================================================="
+                        + "/n*********************************END OF GAME CREDITS*********************************"
+                        + "/n===================================================================================="
+                        + "/nThis game was inspired by various spy movies and stories."
+                        + "/nNo spies were physically injured or killed in the testing of this game.  Except one."
+                        + "/nInitial Development by Kristin Newill, Densie Goetz, Dana Hudrlik"
+                        + "/nImplementation and coding by Jade Jensen and Ruben Gonzalez Flores");
+    }
+    }
