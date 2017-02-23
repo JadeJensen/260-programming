ff637e17409674630465bd413a656803571e71c7
Jade <Jade@DESKTOP-G2F7OO8>
2017-02-22 17:35:28 -0700

    Added all views for game. No views have been developed yet. -- Jade

---
+++ projectSNIPE/src/byui/cit260/projectSNIPE/view/GameOverView.java
@@ -0,0 +1,15 @@
+/*
+ * 
+ * NOTE: THE ORIGINAL PLANS CALLED FOR TWO END USER STORIES, ONE OF A GAME OVER WIN AND A GAME OVER LOSE
+ *       I COMBINED THE VIEWS IN FAVOR OF A SUBSTITUTE IF ELSE STATEMENT:
+ *       IF(WIN == TRUE) THEN ... ELSE LOSE, TRY AGAIN
+ */
+package byui.cit260.projectSNIPE.view;
+
+/**
+ *
+ * @author Jade
+ */
+public class GameOverView {
+    
+}
